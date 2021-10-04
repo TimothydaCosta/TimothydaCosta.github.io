853e4f85f6c8c97a882c456549c68e440f70773a
TimothydaCosta <90334109+TimothydaCosta@users.noreply.github.com>
2021-10-04 14:18:12 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,38 @@
-alert("bam");
+var i = "oui"
+var table = [];
+var somme = 0;
+var y = 0;
+
+while (i === "oui") {
+  table.push(parseInt(prompt("Entrer un nombre")))
+  i = prompt("Voulez-vous continuer? Oui/non");
+}
+
+
+for (y = 0; table.length > y; y++) {
+  somme = somme + table[y];
+}
+
+
+var max = table[0];
+var j = 0;
+
+for (j = 0; table.length >= j; j++) {
+  if (table[j] > max) {
+    max = table[j];
+  }
+}
+
+var min = table[0];
+
+for (j = 0; table.length >= j; j++) {
+  if (table[j] < min) {
+    min = table[j];
+  }
+}
+
+alert("Nombres entrees:" + table);
+alert("Somme=" + somme);
+alert("Moyenne=" + (somme / table.length));
+alert("Min=" + min);
+alert("Max=" + max);
